c290ff9af1fb3254ecc7f55a8776592be7bc19d9
Joe Bentley <joebentley10@gmail.com>
2017-11-08 08:11:24 +0000

    Typeclassopedia progress

--- typeclassopedia/Functors.hs
+++ typeclassopedia/Functors.hs
@@ -18,3 +18,31 @@ instance Functor (Func e) where
 
 testFuncFunctor :: IO ()
 testFuncFunctor = putStrLn . show . fmap (+1) $ Func 5 1
+
+data Tuple e a = Tuple e a deriving Show
+
+instance Functor (Tuple e) where
+    fmap f (Tuple e a) = Tuple e (f a)
+
+data Pair a = Pair a a deriving Show
+
+instance Functor Pair where
+    fmap f (Pair a b) = Pair (f a) (f b)
+
+-- Tuple has kind * -> * -> * whereas Pair has kind * -> *
+-- This makes it possible to define a Functor instance for the type Pair
+-- which is not possible for Tuple (instead we have to implement an instance
+-- for Tuple e). For the Tuple, the first element is left alone, whereas
+-- in the Pair both elements are modified.
+
+data ITree a = Leaf (Int -> a) | Node [ITree a]
+
+instance Functor ITree where
+    fmap :: (a -> b) -> ITree a -> ITree b
+    fmap f (Leaf g) = Leaf $ f . g
+    fmap f (Node x) = Node . map (fmap f) $ x
+
+data Compose f g x = Compose { unComp :: f (g x) }
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+    fmap f = Compose . fmap (fmap f) . unComp
