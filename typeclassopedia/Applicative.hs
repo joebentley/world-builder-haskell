e118f9e9e670e2b719b3b036a2adfc46e7d4b6a1
Joe Bentley <joebentley10@gmail.com>
2017-11-08 20:59:02 +0000

    Applicative

--- typeclassopedia/Applicative.hs
+++ typeclassopedia/Applicative.hs
@@ -1,3 +1,26 @@
+{-# LANGUAGE InstanceSigs #-}
 
 data Maybe' a = Nothing' | Just' a deriving Show
 
+instance Functor Maybe' where
+    fmap f (Just' a) = Just' (f a)
+    fmap _ Nothing' = Nothing'
+
+instance Applicative Maybe' where
+    pure = Just'
+
+    _ <*> Nothing' = Nothing'
+    Nothing' <*> _ = Nothing'
+    Just' f <*> Just' a = Just' $ f a
+
+newtype ZipList a = ZipList { getZipList :: [a] } deriving Show
+
+instance Functor ZipList where
+    fmap f = ZipList . fmap f . getZipList
+
+instance Applicative ZipList where
+    pure :: a -> ZipList a
+    pure a = ZipList [a]   -- exercise
+
+    (<*>) :: ZipList (a -> b) -> ZipList a -> ZipList b
+    (ZipList gs) <*> (ZipList xs) = ZipList (zipWith ($) gs xs)
