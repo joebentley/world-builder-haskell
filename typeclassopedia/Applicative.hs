118d8b0eb78b3e715f3e321099f696d5dea05e32
Joe Bentley <joebentley10@gmail.com>
2017-11-12 12:13:02 +0000

    Applicative stuff

--- typeclassopedia/Applicative.hs
+++ typeclassopedia/Applicative.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE InstanceSigs #-}
 
+import Control.Applicative (liftA2)
+
 data Maybe' a = Nothing' | Just' a deriving Show
 
 instance Functor Maybe' where
@@ -20,7 +22,21 @@ instance Functor ZipList where
 
 instance Applicative ZipList where
     pure :: a -> ZipList a
-    pure a = ZipList [a]   -- exercise
+    pure a = ZipList (repeat a) -- infinite list of a
 
     (<*>) :: ZipList (a -> b) -> ZipList a -> ZipList b
     (ZipList gs) <*> (ZipList xs) = ZipList (zipWith ($) gs xs)
+
+sequenceAL :: Applicative f => [f a] -> f [a]
+sequenceAL = foldr (\x ys -> liftA2 (:) x ys) $ pure []
+
+class Functor f => Monoidal f where
+    unit :: f ()
+    (****) :: f a -> f b -> f (a,b)
+
+class Monoidal f => Applicative' f where
+    -- pure' :: a -> f a
+    -- pure' a = 
+
+    (<**>) :: f (a -> b) -> f a -> f b
+    (<**>) g x = fmap (\x -> fst x $ snd x) . (g **** x)
