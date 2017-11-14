b0f598a5ad65ca1c8941378d1cb69ed4687284ef
Joe Bentley <joebentley10@gmail.com>
2017-11-14 08:01:25 +0000

    Moved file

--- wikibooks-monads/prologue.hs
+++ wikibooks-monads/prologue.hs
@@ -11,10 +11,16 @@ main = do
 main2 :: IO ()
 main2 = do
     putStrLn "Please enter a number"
-    a1 <- getLine
+    a1 <- readMaybe <$> getLine
     putStrLn "Please enter another number"
-    a2 <- getLine
+    a2 <- readMaybe <$> getLine
 
-    case (+) <$> (readMaybe a1) <*> (readMaybe a2) of
+    case (+) <$> a1 <*> a2 of
         Just x -> putStrLn ("The sum of your numbers is " ++ show x)
         Nothing -> putStrLn "Could not read number"
+
+interactiveConcatenating :: IO ()
+interactiveConcatenating = do
+    putStrLn "Choose two strings:"
+    s <- (++) <$> getLine <*> getLine
+    putStrLn "Let's concatenate them:" *> putStrLn s
